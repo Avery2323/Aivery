488dccf19340b19cbfe6ac3f568fc035709496b8
Robyn <jadepretzel66@gmail.com>
2020-06-08 16:18:49 +1000

    0.0.0.2
    - Proper Internal versioning
    - Tidied up several lines, to keep the program readable to humans

--- AIvery.py
+++ AIvery.py
@@ -13,10 +13,11 @@ def line(text):
 
 pronountype_a = ['he/him/his', "she/her/hers", "it/its", "fleur/fleurs"]
 pronountype_b = ["they/them/theirs", "fae/faer/faers", "fey/fem/feirs"]
-pronouns = ['he/him/his', "she/her/hers", "it/its", "fleur/fleurs", "they/them/theirs", "fae/faer/faers", "fey/fem/feirs"]
+pronouns = ['he/him/his', "she/her/hers", "it/its", "fleur/fleurs",
+"they/them/theirs", "fae/faer/faers", "fey/fem/feirs"]
 
 line("Greetings!")
-line("AIvery 0.0.0 loaded.")
+line("AIvery 0.0.0.2 loaded.")
 line("Please enter your name.")
 username = input(">>> ")
 line(f"Is {username} your name?")
@@ -34,8 +35,10 @@ while confirm.lower() != 'y':
     confirm = input("Y/N>>> ")
 
 line("It is time to choose a set of pronouns, to make your experience more pleasant.")
-line("At present, I have He/Him/His, She/Her/Hers, They/Them/Theirs, and It/Its in my database.")
-line("In addition to this, I am also familiar with a few sets of neopronouns. Would you like to view them?")
+line("At present, I have He/Him/His, She/Her/Hers, They/Them/Theirs, and"
+" It/Its in my database.")
+line("In addition to this, I am also familiar with a few sets of neopronouns."
+" Would you like to view them?")
 answer = input("Y/N>>> ")
 while answer.lower() not in "yn":
     line("Error One: Invalid Input.")
@@ -43,14 +46,16 @@ while answer.lower() not in "yn":
     answer = input("Y/N>>> ")
 
 if answer.lower() == 'y':
-    line("Available neopronoun sets are: Fae/Faer/Faers, Fey/Fem/Feirs, Fleur/Fleurs.")
+    line("Available neopronoun sets are: Fae/Faer/Faers, Fey/Fem/Feirs,"
+    " Fleur/Fleurs.")
     line("If your pronouns are not in this list, please contact the developer")
 
 line("Please select a set of pronouns.")
 pronoun_select = input(">>> ")
 while pronoun_select.lower() not in pronouns:
     line("Error Two: Pronouns not recognised.")
-    line("If you wish for your pronouns to be included in my database, please contact the developer.")
+    line("If you wish for your pronouns to be included in my database, please"
+    " contact the developer.")
     line("Please enter a set of pronouns")
     pronoun_select = input(">>> ")
 user_pronouns = pronoun_select
@@ -64,5 +69,7 @@ while True:
         line("For ease of use, however, I use the pronouns they/them.")
     else:
         line(f"I am afraid I do not understand, {username}")
-        line("Please try wording the query differently, or checking the list of available queries.")
-        line("If you think there is an error or mistake, please report this to the developer.")
+        line("Please try wording the query differently, or checking the list"
+        " of available queries.")
+        line("If you think there is an error or mistake, please report this"
+        " to the developer.")
